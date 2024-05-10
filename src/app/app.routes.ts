af50d937baf56d0a20edf99e8bd25c94ee6ceffc
María Victoria <victoriacursos2022@gmail.com>
2024-05-10 15:06:49 -0300

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsDetailComponent } from './pages/products-detail/products-detail.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { WeLoveComponent } from './pages/we-love/we-love.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'we-love' , component: WeLoveComponent},
+  {path: 'products/:id', component: ProductsDetailComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: '', pathMatch:'full'}
+
+];
---
+++ src/app/pages/we-love/we-love.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-we-love',
+  standalone: true,
+  imports: [],
+  templateUrl: './we-love.component.html',
+  styleUrl: './we-love.component.css'
+})
+export class WeLoveComponent {
+
+}
